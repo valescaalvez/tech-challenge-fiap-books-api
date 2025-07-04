a5ad298185bdada0a2128d5681550116d8e0b77b
valescaalvez <jujubaworks@gmail.com>
2025-07-04 07:54:11 -0300

    init update

---
+++ api/__init__.py
@@ -0,0 +1,12 @@
+from flask_jwt_extended import JWTManager
+
+def create_app():
+    app = Flask(__name__)
+    app.config['JWT_SECRET_KEY'] = 'sua-chave-super-secreta'  
+    
+    Swagger(app)
+    JWTManager(app)
+
+    from .routes import bp
+    app.register_blueprint(bp)
+    return app
