412a40a53ea8927fa82a43bf3eb894edb7b2c719
valescaalvez <jujubaworks@gmail.com>
2025-07-05 20:04:24 -0300

    init update

--- api/__init__.py
+++ api/__init__.py
@@ -1,5 +1,7 @@
 from flask_jwt_extended import JWTManager
 
+
+
 def create_app():
     app = Flask(__name__)
     app.config['JWT_SECRET_KEY'] 
@@ -7,6 +9,10 @@ def create_app():
     Swagger(app)
     JWTManager(app)
 
-    from .routes import bp
-    app.register_blueprint(bp)
+    from .routes import bp # Principal
+    from .auth_routes import auth_bp #autenticação
+    
+
+    app.register_blueprint(bp) # Registra as rotas da API principal
+    app.register_blueprint(auth_bp) # Registra as rotas de autenticação
     return app
