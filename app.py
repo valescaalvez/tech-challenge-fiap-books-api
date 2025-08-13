be1b10e62f9d74f7b5918c8f0acfdabedc28b72f
Felipe B. <99092931+fbreseghello@users.noreply.github.com>
2025-08-12 21:16:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+import os
 from api import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Porta dinâmica do Render
+    app.run(host="0.0.0.0", port=port, debug=True)
